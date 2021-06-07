ed4d3185da998359bda2c4b8021bcc6491d1c745
Rithick <rithick0907@gmail.com>
2021-06-07 08:55:28 +0530

    Exponential Search is implemented

---
+++ src/searching/ExponentialSearch.java
@@ -0,0 +1,34 @@
+package searching;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// Time Complexity = O(logi).
+// Restriction : Input Array must be sorted.
+// Logic:
+// 1. In Exponential Search, we start with small range and see if the target item is there or not.
+// 2. If not, then we double the range at each step.
+// 3. Once we find the range where target item exist, we do binarySearch in that range.
+// Note:  If you have N elements and if target is in the first sqrt(N) elements,
+//        then exponential search will be faster, else binary search will be faster.
+
+public class ExponentialSearch {
+    public static int exponentialSearch(int[] nums, int target) {
+        int bound = 1;
+        while(bound < nums.length && nums[bound] < target)
+            bound *= 2;
+
+        int start = bound / 2;
+        int end = Math.min(bound,nums.length);
+
+        return BinarySearchIterative.binarySearch(nums,target,start,end);
+    }
+
+    public static void main(String[] args) throws Exception {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        int[] nums = {10, 15, 20, 25, 30, 35, 37, 40};
+        int target = Integer.parseInt(br.readLine());
+
+        System.out.println(exponentialSearch(nums, target));
+    }
+}

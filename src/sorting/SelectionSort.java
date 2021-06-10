a27f5ee778c05ef66b7a4a406662147e378763d3
Rithick <rithick0907@gmail.com>
2021-06-10 16:52:45 +0530

    Selection sort implemented

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -1,11 +1,14 @@
 package sorting;
 
 import java.util.Arrays;
+
 /*
-Logic: At the end of each iteration, the next largest item bubble up & moves to it correct position.
 Time Complexity : O(n^2)
+Logic:
+ 1. It's like an inverse of Bubble Sort.
+ 2. Here instead of bubble up largest value to its correct position, we select smallest value and moves it to its correct position.
 */
-public class BubbleSort {
+public class SelectionSort {
     private static void swap(int[] nums, int index1, int index2) {
         int temp = nums[index1];
         nums[index1] = nums[index2];
@@ -14,10 +17,12 @@ public class BubbleSort {
 
     public static void sort(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 1; j < nums.length - i; j++) {
-                if (nums[j] < nums[j - 1])
-                    swap(nums, j, j - 1);
+            int minIndex = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[minIndex])
+                    minIndex = j;
             }
+            swap(nums,i,minIndex);
         }
     }
 
